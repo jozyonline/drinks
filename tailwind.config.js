ab855f1e3739cf61ac96f720ec81c68f2d26f63e
jozy04 <jozyonline04@gmail.com>
2023-01-08 23:27:46 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero': "url('https://fastly.4sqi.net/img/general/600x600/8170960_pANL3hAVnpcqCXQ0upRwHoiAGIIKRW-dKm6TFke3yUY.jpg')", 
+      }
+
+    },
+  },
+  plugins: [],
+}
